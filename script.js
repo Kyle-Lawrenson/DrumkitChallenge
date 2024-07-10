c701c5a9ea5e6b5fc957ef2c68a8b5f9cbb2ca69
Kyle Lawrenson <kylelawrenson99@gmail.com>
2024-07-10 13:53:07 +0100

    finshed buttons

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
 console.log("hello")
 
 const boomButton = document.getElementById("boomButton")
-const clapButton = document.getElementById("clapButton")
-const hihatButton = document.getElementById("hihatButton")
-const kickButton = document.getElementById("kickButton")
-const openhatButton = document.getElementById("openhatButton")
-const rideButton = document.getElementById("rideButon")
-const snareButton = document.getElementById("snareButton")
-const tinkButton = document.getElementById("tinkButton")
-const tomButton = document.getElementById("tomButton")
-
 let boom = new Audio("./sounds/boom.wav")
-let clap = new Audio("./sounds/clap.wav")
-let hihat = new Audio("./sounds/hihat.wav")
-let kick = new Audio("./sounds/kick.wav")
-let openhat = new Audio("./sounds/openhat.wav")
-let ride = new Audio("./sounds/ride.wav")
-let snare = new Audio("./sounds/snare.wav")
-let tink = new Audio("./sounds/tink.wav")
-let tom = new Audio("./sounds/tom.wav")
-
-
-
 document.addEventListener('keydown', (event) => {
     if (event.key === 'w' || event.key === 'W') {
         boom.play()
@@ -30,17 +10,92 @@ boomButton.addEventListener('click', () => {
     boom.play()
 });
 
+const clapButton = document.getElementById("clapButton")
+let clap = new Audio("./sounds/clap.wav")
 document.addEventListener('keydown', (event) => {
     if (event.key === 'a' || event.key === 'A') {
         clap.play()
-    }
-})
-
-
+    }})
 clapButton.addEventListener('click', () => {
     clap.play()
 });
 
+const hihatButton = document.getElementById("hihatButton")
+let hihat = new Audio("./sounds/hihat.wav")
+document.addEventListener('keydown', (event) => {
+    if (event.key === 's' || event.key === 'S') {
+        hihat.play()
+    }
+})
+hihatButton.addEventListener('click', () => {
+    hihat.play()
+});
+
+const kickButton = document.getElementById("kickButton")
+let kick = new Audio("./sounds/kick.wav")
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'd' || event.key === 'D') {
+        kick.play()
+    }
+})
+kickButton.addEventListener('click', () => {
+    kick.play()
+});
+
+const openhatButton = document.getElementById("openhatButton")
+let openhat = new Audio("./sounds/openhat.wav")
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'q' || event.key === 'Q') {
+        openhat.play()
+    }
+})
+openhatButton.addEventListener('click', () => {
+    openhat.play()
+});
+
+const rideButton = document.getElementById("rideButon")
+let ride = new Audio("./sounds/ride.wav")
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'e' || event.key === 'E') {
+        ride.play()
+    }
+})
+rideButton.addEventListener('click', () => {
+    ride.play()
+});
+
+const snareButton = document.getElementById("snareButton")
+let snare = new Audio("./sounds/snare.wav")
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'z' || event.key === 'Z') {
+        snare.play()
+    }
+})
+snareButton.addEventListener('click', () => {
+    snare.play()
+});
+
+const tinkButton = document.getElementById("tinkButton")
+let tink = new Audio("./sounds/tink.wav")
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'x' || event.key === 'X') {
+        tink.play()
+    }
+})
+tinkButton.addEventListener('click', () => {
+    tink.play()
+});
+
+const tomButton = document.getElementById("tomButton")
+let tom = new Audio("./sounds/tom.wav")
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'c' || event.key === 'C') {
+        tom.play()
+    }
+})
+tomButton.addEventListener('click', () => {
+    tom.play()
+});
 
 
 // asked chatGPT how to do add sound on specific keydown (start)
