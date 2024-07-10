ae32a851f8c6210b87fe1178174a75a4d0acf78d
Kyle Lawrenson <kylelawrenson99@gmail.com>
2024-07-10 11:17:43 +0100

    add some to script

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 const boom = document.getElementById("boom")
 const clap = document.getElementById("clap")
-const hiHat = document.getElementById("hiHat")
+const hihat = document.getElementById("hihat")
 const kick = document.getElementById("kick")
-const openHat = document.getElementById("openHat")
+const openhat = document.getElementById("openhat")
 const ride = document.getElementById("ride")
 const snare = document.getElementById("snare")
 const tink = document.getElementById("tink")
 const tom = document.getElementById("tom")
 
+
+document.addEventListener("keydown", (event) => {
+    boom.textContent = event.boom
+    clap.textContent = event.clap
+    hihat.textContent = event.hihat
+    kick.textContent = event.kick
+    openhat.textContent = event.openhat
+    ride.textContent = event.ride
+    snare.textContent = event.snare
+    tink.textContent = event.tink
+    tom.textContent = event.tom
+} )
+
 document.addEventListener("click", (event) => {
     boom.textContent = event.boom
     clap.textContent = event.clap
-    hiHat.textContent = event.hiHat
+    hihat.textContent = event.hihat
     kick.textContent = event.kick
-    openHat.textContent = event.openHat
+    openhat.textContent = event.openhat
     ride.textContent = event.ride
     snare.textContent = event.snare
     tink.textContent = event.tink
